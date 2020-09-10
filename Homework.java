463ac99d3c04809fcb1ea956ab58bc28823a2986
SeanClancyRyerson <seanclancyryerson@gmail.com>
2020-09-10 10:30:43 -0400

    added hw1 to github

---
+++ Homework.java
@@ -0,0 +1,17 @@
+public class Homework {
+	public static void main(String[] args) {
+		// If it matches my sample exactly it gets a 0
+		// 1. Create a variable that holds a name
+		String name = "Sean Ryerson";
+		// 2. Output the name to the console
+		System.out.println("Your name is: " + name);
+		// 3. Create a variable that holds an age (as a whole number)
+		int age = 22;
+		// 4. Output the age to the console
+		System.out.println(name + "\'s age is: " + age);
+		// 5. Create a variable that holds a decimal
+		float dec = 6.9f;
+		// 6. Output the variable
+		System.out.println("A funny decimal is: " + dec);
+	}
+}
